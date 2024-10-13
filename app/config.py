ea139ff3a4b9ba73e8621d7664fdec191e41fb1b
Retr0413 <153156670+Retr0413@users.noreply.github.com>
2024-10-13 20:40:48 +0900

    Revert "データベースに対する保存とテーブルの作成に成功、次はラベルをうまくつける。"
    This reverts commit 626d30427443e802703f5ff7df86b898d9dd502e.

--- app/config.py
+++ app/config.py
@@ -1,17 +1,9 @@
 import os
-from dotenv import load_dotenv
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Lita0601')
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'mysql+mysqlconnector://user:password@db/database'
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+mysqlconnector://your_user:your_password@db/your_database')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/app/uploads')
-
     ALLOWED_EXTENSIONS = {'pdf'}
-
-    load_dotenv()
